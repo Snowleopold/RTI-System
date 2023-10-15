e8c4dfa08ed8a6acb321b748c42f5794a592fd2c
Snowleopold <113206098+Snowleopold@users.noreply.github.com>
2023-10-15 18:35:16 +0800

    Add files via upload

---
+++ emitter.ino
@@ -0,0 +1,115 @@
+#include <WiFi.h>
+#include <esp_now.h>
+
+const int total=16;
+bool found=false;
+bool check=false;
+int n;
+int num=7;
+bool receive=false;
+const char* boosted="boost";
+const char* tran="transfer";
+const char* checkmsg="check";
+const char* ssid1="node1";
+const char* ssid2="node2";
+const char* ssid3="node3";
+const char* ssid4="node4";
+const char* ssid5="node5";
+const char* ssid6="node6";
+const char* ssid7="node7";
+const char* ssid8="node8";
+const char* ssid9="node9";
+const char* ssid10="node10";
+const char* ssid11="node11";
+const char* ssid12="node12";
+const char* ssid13="node13";
+const char* ssid14="node14";
+const char* ssid15="node15";
+const char* ssid16="node16";
+const char* pass="53732394";
+uint8_t base[]={0xB0,0xA7,0x32,0x2A,0x68,0x6C};
+const char* emitterlist[]={ssid1,ssid2,ssid3,ssid4,ssid5,ssid6,ssid7,ssid8,ssid9,ssid10,ssid11,ssid12,ssid13,ssid14,ssid15,ssid16};
+int size=total-1;
+const char** ssidlist;
+const char* route1[]={ssid2,ssid3,ssid4,ssid5,ssid6,ssid7,ssid8,ssid9,ssid10,ssid11,ssid12,ssid13,ssid14,ssid15,ssid16};
+const char* route2[]={ssid1,ssid3,ssid4,ssid5,ssid6,ssid7,ssid8,ssid9,ssid10,ssid11,ssid12,ssid13,ssid14,ssid15,ssid16};
+const char* route3[]={ssid1,ssid2,ssid4,ssid5,ssid6,ssid7,ssid8,ssid9,ssid10,ssid11,ssid12,ssid13,ssid14,ssid15,ssid16};
+const char* route4[]={ssid1,ssid2,ssid3,ssid5,ssid6,ssid7,ssid8,ssid9,ssid10,ssid11,ssid12,ssid13,ssid14,ssid15,ssid16};
+const char* route5[]={ssid1,ssid2,ssid3,ssid4,ssid6,ssid7,ssid8,ssid9,ssid10,ssid11,ssid12,ssid13,ssid14,ssid15,ssid16};
+const char* route6[]={ssid1,ssid2,ssid3,ssid4,ssid5,ssid7,ssid8,ssid9,ssid10,ssid11,ssid12,ssid13,ssid14,ssid15,ssid16};
+const char* route7[]={ssid1,ssid2,ssid3,ssid4,ssid5,ssid6,ssid8,ssid9,ssid10,ssid11,ssid12,ssid13,ssid14,ssid15,ssid16};
+const char* route8[]={ssid1,ssid2,ssid3,ssid4,ssid5,ssid6,ssid7,ssid9,ssid10,ssid11,ssid12,ssid13,ssid14,ssid15,ssid16};
+const char* route9[]={ssid1,ssid2,ssid3,ssid4,ssid5,ssid6,ssid7,ssid8,ssid10,ssid11,ssid12,ssid13,ssid14,ssid15,ssid16};
+const char* route10[]={ssid1,ssid2,ssid3,ssid4,ssid5,ssid6,ssid7,ssid8,ssid9,ssid11,ssid12,ssid13,ssid14,ssid15,ssid16};
+const char* route11[]={ssid1,ssid2,ssid3,ssid4,ssid5,ssid6,ssid7,ssid8,ssid9,ssid10,ssid12,ssid13,ssid14,ssid15,ssid16};
+const char* route12[]={ssid1,ssid2,ssid3,ssid4,ssid5,ssid6,ssid7,ssid8,ssid9,ssid10,ssid11,ssid13,ssid14,ssid15,ssid16};
+const char* route13[]={ssid1,ssid2,ssid3,ssid4,ssid5,ssid6,ssid7,ssid8,ssid9,ssid10,ssid11,ssid12,ssid14,ssid15,ssid16};
+const char* route14[]={ssid1,ssid2,ssid3,ssid4,ssid5,ssid6,ssid7,ssid8,ssid9,ssid10,ssid11,ssid12,ssid13,ssid15,ssid16};
+const char* route15[]={ssid1,ssid2,ssid3,ssid4,ssid5,ssid6,ssid7,ssid8,ssid9,ssid10,ssid11,ssid12,ssid13,ssid14,ssid16};
+const char* route16[]={ssid1,ssid2,ssid3,ssid4,ssid5,ssid6,ssid7,ssid8,ssid9,ssid10,ssid11,ssid12,ssid13,ssid14,ssid15};
+const char** routelist[]={route1,route2,route3,route4,route5,route6,route7,route8,route9,route10,route11,route12,route13,route14,route15,route16};
+
+typedef struct packet{
+  char info[32];
+  short value[total-1];
+} packet;
+
+packet datapack,sendpack,dummy;
+esp_now_peer_info_t peerinfo;
+
+void ondatarecv(const uint8_t* mac,const uint8_t* incomingdata,int len){
+  memcpy(&dummy,incomingdata,sizeof(dummy));
+  if(strcmp(dummy.info,checkmsg)==0) check=true;
+  else{
+    receive=true;
+  }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  WiFi.mode(WIFI_AP_STA);
+  Serial.begin(115200);
+  esp_now_init();
+  esp_now_register_recv_cb(ondatarecv);
+  memcpy(peerinfo.peer_addr,base,6);
+  peerinfo.channel=0;
+  peerinfo.encrypt=false;
+  esp_now_add_peer(&peerinfo);
+  ssidlist=routelist[num-1];
+  strcpy(sendpack.info,boosted);
+  strcpy(datapack.info,tran);
+  esp_now_send(base,(uint8_t*) &sendpack,sizeof(sendpack));
+  WiFi.softAP(emitterlist[num-1],pass,1);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(receive){
+    n=WiFi.scanNetworks(false,false,false,500,1);
+    int tim=0;
+    while(tim<size){
+      // Serial.println("trying");
+      for(int i=0;i<n;i++){
+        // Serial.print(WiFi.SSID(i));
+        // Serial.print("    ");
+        // Serial.println(ssidlist[tim]);
+        if(strcmp(WiFi.SSID(i).c_str(),ssidlist[tim])==0){
+          datapack.value[tim++]=WiFi.RSSI(i);
+          found=true;
+          break;
+        }
+      }
+      if(found) found=false;
+      else n=WiFi.scanNetworks(false,false,false,500,1);
+    }
+    tim=0;
+    while(!check){
+      delay(100);
+    };
+    WiFi.enableAP(false);
+    esp_now_send(base,(uint8_t*) &datapack,sizeof(datapack));
+    receive=false;
+    check=false;
+    WiFi.enableAP(true);
+  }
+}
---
+++ station.ino
@@ -0,0 +1,143 @@
+#include <WiFi.h>
+#include <esp_now.h>
+
+int done=0;
+int nodenum;
+const int total=16;
+int count=0;
+int expect;
+int t1,t2;
+const char* boosted="boost";
+const char* tran="transfer";
+const char* win1="Station ";
+const char* win2=" boosted!";
+const char* checkmsg="check";
+uint8_t emitter1[]={0xA8,0x42,0xE3,0xCA,0x89,0xAC};
+uint8_t emitter2[]={0xB0,0xA7,0x32,0xF3,0x96,0x68};
+uint8_t emitter3[]={0xB0,0xA7,0x32,0x29,0xF0,0x50};
+uint8_t emitter4[]={0xA8,0x42,0xE3,0xCA,0x23,0xBC};
+uint8_t emitter5[]={0xA8,0x42,0xE3,0xCB,0x78,0x14};
+uint8_t emitter6[]={0xA8,0x42,0xE3,0xCB,0x99,0xF4};
+uint8_t emitter7[]={0xB0,0xA7,0x32,0xF2,0xEE,0x14};
+uint8_t emitter8[]={0xA8,0x42,0xE3,0xCB,0x2F,0x00};
+uint8_t emitter9[]={0xB0,0xA7,0x32,0x29,0xF1,0xD8};
+uint8_t emitter10[]={0xA8,0x42,0xE3,0xCB,0x35,0x9C};
+uint8_t emitter11[]={0xA8,0x42,0xE3,0xCA,0x79,0x98};
+uint8_t emitter12[]={0xA8,0x42,0xE3,0xCA,0x9F,0xF8};
+uint8_t emitter13[]={0xB0,0xA7,0x32,0xF2,0x9D,0xC8};
+uint8_t emitter14[]={0xA8,0x42,0xE3,0xCA,0x75,0x10};
+uint8_t emitter15[]={0xA8,0x42,0xE3,0xCB,0x77,0xC4};
+uint8_t emitter16[]={0xB0,0xA7,0x32,0x2A,0xD2,0x70};
+uint8_t* emitterlist[]={emitter1,emitter2,emitter3,emitter4,emitter5,emitter6,emitter7,emitter8,emitter9,emitter10,emitter11,emitter12,emitter13,emitter14,emitter15,emitter16};
+short values[total*(total-1)];
+
+bool maccompare(const uint8_t* mac1,const uint8_t* mac2,int len){
+  for(int i=0;i<len;i++){
+    if(mac1[i]!=mac2[i]) return false;
+  }
+  return true;
+}
+
+typedef struct packet{
+  char info[32];
+  short value[total-1];
+} packet;
+
+packet recvpack,sendpack,checkpack;
+esp_now_peer_info_t peerinfo;
+
+void ondatarecv(const uint8_t* mac,const uint8_t* incomingdata,int len){
+  memcpy(&recvpack,incomingdata,sizeof(recvpack));
+  if(strcmp(boosted,recvpack.info)==0){
+    count++;
+    // Serial.println(count);
+    // Serial.print(++count);
+    // Serial.print('/');
+    // Serial.println(total);
+    if(count==total){
+      // Serial.println("All nodes set!");
+      count=0;
+      t1=millis();
+      esp_now_send(0,(uint8_t*) &sendpack,sizeof(sendpack));
+      // Serial.println("Sent packet to all nodes");
+      delay(500);
+      esp_now_send(emitter1,(uint8_t*) &checkpack,sizeof(checkpack));
+      // Serial.println("Sent check packet to node#1");
+      expect=1;
+      Serial.write('s');
+    }
+  }
+  else if(strcmp(tran,recvpack.info)==0){
+    for(int i=1;i<=total;i++){
+      if(maccompare(emitterlist[i-1],mac,6)){
+        // Serial.print("Packet from node#");
+        // Serial.print(i);
+        // Serial.println(" is received");
+        if(i==expect){
+          nodenum=i;
+        }
+        else{
+          return;
+        }
+        break;
+      }
+    }
+    for(int i=0;i<(total-1);i++){
+      values[done++]=(-recvpack.value[i]);
+    }
+    // Serial.print("Number of data collected: ");
+    // Serial.println(done);
+    if(nodenum==total){
+      done=0;
+      t2=millis();
+      // Serial.println("All data collected");
+      // Serial.print("Data: ");
+      for(int i=0;i<((total)*(total-1));i++){
+        // Serial.print(values[i]);
+        Serial.write(values[i]);
+        if(i==((total)*(total-1)-1)){
+          // Serial.println();
+        }
+        else{
+          // Serial.print("  ");
+        }
+      }
+      // Serial.print("Time required: ");
+      // Serial.println(t2-t1);
+      t1=t2;
+      esp_now_send(0,(uint8_t*) &sendpack,sizeof(sendpack));
+      // Serial.println("Sent packet to all nodes");
+      delay(500);
+      esp_now_send(emitter1,(uint8_t*) &checkpack,sizeof(checkpack));
+      // Serial.println("Sent check packet to node#1");
+      expect=1;
+      Serial.write('s');
+    }
+    else{
+      esp_now_send(emitterlist[nodenum],(uint8_t*) &checkpack,sizeof(checkpack));
+      // Serial.print("Sent check packet to node#");
+      // Serial.println(nodenum+1);
+      expect=nodenum+1;
+    }
+  }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  esp_now_init();
+  esp_now_register_recv_cb(ondatarecv);
+  strcpy(sendpack.info,tran);
+  strcpy(checkpack.info,checkmsg);
+  peerinfo.channel=0;
+  peerinfo.encrypt=false;
+  for(int i=0;i<total;i++){
+    memcpy(peerinfo.peer_addr,emitterlist[i],6);
+    esp_now_add_peer(&peerinfo);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
